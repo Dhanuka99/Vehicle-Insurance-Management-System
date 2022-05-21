3d96a9912c32f4732adbf41cf801b17075246ac7
DHANUKA <danuka1999lakshan@gmail.com>
2022-05-21 23:52:16 +0530

    admin part

--- php/login.php
+++ php/login.php
@@ -1,12 +1,16 @@
+<?php
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <link rel="stylesheet" href="../css/login.css">
+    <title>Document</title>
 </head>
 <body>
-    
+    <img src="../img/wave.svg" alt="">
 </body>
 </html>
